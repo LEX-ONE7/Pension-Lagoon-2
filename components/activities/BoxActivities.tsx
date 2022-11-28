681e21f6ce365d007ba20900c18bd53c2823e66f
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-28 11:50:35 -1000

    1 file modified

--- components/activities/BoxActivities.tsx
+++ components/activities/BoxActivities.tsx
@@ -3,7 +3,19 @@ const BoxActivities = () => (
   <div className="container">
     <div className="row">
       <h2>Activités</h2>
-      
+      <div className="card text-center">
+        <div className="card-header">Sur Mer</div>
+        <div className="card-body">
+          <h5 className="card-title"></h5>
+          <p className="card-text">
+            
+          </p>
+          <a href="#" className="btn btn-primary">
+            Réserver
+          </a>
+        </div>
+        <div className="card-footer text-muted">Kayak</div>
+      </div>
     </div>
   </div>
 );
