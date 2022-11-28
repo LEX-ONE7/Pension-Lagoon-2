e49f0c8d326467a79d6c41d74a9b3ad9150da321
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-28 12:23:01 -1000

    1 file modified

--- components/activities/BoxActivities.tsx
+++ components/activities/BoxActivities.tsx
@@ -1,23 +1,35 @@
 import * as React from 'react';
 const BoxActivities = () => (
-  <div className="container">
+  <div className="container-fluid">
     <div className="row">
-      <h2>Activités</h2>
-      <div className="card text-center">
-        <div className="card-header">Sur Mer</div>
-        <div className="card-body">
-          <h5 className="card-title"></h5>
-          <p className="card-text">
-            Choisissez un jet ski simple ou double et faites le tour du paradis
-            polynésien avec un guide expérimenté
-          </p>
-          <a href="#" className="btn btn-primary">
-            Réserver
-          </a>
-        </div>
-        <div className="card-footer text-muted">Jet Ski</div>
+      <div className="col-12 col-md-8 bg-success">
+        <h3> Activités Nautiques</h3>
+
+        <p>
+          {' '}
+          Entouré par les eaux cristallines de l'océan Pacifique Sud,
+          l'InterContinental Tahiti Resort & Spa propose une mutitude de sports
+          nautiques. Offrez-vous une excursion guidée de plongée en apnée ou une
+          sortie en voilier, partez à l'aventure pour surfer les vagues de
+          Tahiti ou faites le plein de sensations aquatiques en kayak ou en jet
+          ski.{' '}
+        </p>
       </div>
     </div>
+    ...
   </div>
 );
 export default BoxActivities;
+<div className="card" style="width: 18rem;">
+    <img src="..." className="card-img-top" alt="..." />
+    <div className="card-body">
+      <h5 className="card-title">Jet Ski</h5>
+      <p className="card-text">
+        Choisissez un jet ski simple ou double et faites le tour du paradis
+        polynésien avec un guide expérimenté
+      </p>
+      <a href="#" className="btn btn-primary">
+        Reserver
+      </a>
+    </div>
+  </div>
