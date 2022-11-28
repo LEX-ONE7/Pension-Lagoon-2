c5314c4946123e249fb1015e5148ef2e16e29987
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-28 13:52:30 -1000

    1 file modified

--- components/activities/BoxActivities.tsx
+++ components/activities/BoxActivities.tsx
@@ -3,7 +3,7 @@ const BoxActivities = () => (
   <div className="container-fluid">
     <div className="row">
       <div className="col-12 col-md-8 bg-success">
-        <h3> Activités Nautiques</h3>
+        <h3> Nautiques</h3>
 
         <p>
           {' '}
@@ -13,6 +13,19 @@ const BoxActivities = () => (
           sortie en voilier, partez à l'aventure pour surfer les vagues de
           Tahiti ou faites le plein de sensations aquatiques en kayak ou en jet
           ski.{' '}
+        </p>
+      </div>
+      <div className="col-12 col-md-8 bg-info">
+        <h4>Terrestres</h4>
+
+        <p>
+          {' '}
+          Tahiti est une île regorgeant de trésors cachés, de ses vallées
+          verdoyantes à ses lagons scintillants. Pour découvrir l'île sous tous
+          ses aspects, nous proposons des excursions tous terrains, pour tous
+          les niveaux de forme physique. Faites le tour de l'île accompagné d'un
+          guide, explorez Tahiti en 4x4, partez à l'aventure hors des sentiers
+          battus ou en promenade sur les chemins balisés à travers la jungle.{' '}
         </p>
       </div>
     </div>
