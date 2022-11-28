0dcbbf82f16c1d734544e3ef0e4ef8bfc357a59f
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-28 00:20:30 -1000

    1 file modified

--- components/rooms/BoxBungalow.tsx
+++ components/rooms/BoxBungalow.tsx
@@ -2,14 +2,18 @@ import * as React from 'react';
 import SliderAuto from '../SliderAuto';
 
 const BoxBungalow = () => (
-  <div className="container-fluid">
+  <div className="container-fluid ">
     <div className="row">
-      <div className="col-12 col-md-8 ">
+      <div className="col-12 col-md-8 bg-success">
         <h2>Bungalows & Chambres</h2>
         <SliderAuto />
       </div>
-      <div className="col-12 col-md-8 ">
-        <h2>Bungalows et Chambres</h2>
+      <div className="col-12 col-md-8 bg-info ">
+        <p>Bungalows</p>
+        <SliderAuto />
+      </div>
+      <div className="col-12 col-md-8 bg-warning">
+        <h4>Chambres</h4>
         <SliderAuto />
       </div>
     </div>
