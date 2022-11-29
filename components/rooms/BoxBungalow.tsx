f64f08d066a908d26eed0451dd6f80ff022c7e03
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-29 12:40:41 -1000

    1 file modified

--- components/rooms/BoxBungalow.tsx
+++ components/rooms/BoxBungalow.tsx
@@ -6,9 +6,7 @@ const BoxBungalow = () => (
     <div className="row">
       <div className="col-12  bg-success">
         <h2>Bungalows & Chambres</h2>
-        <p>
-       
-        </p>
+        <p></p>
         <p>
           Equipées d’un TV, d’un mini-bar, de WC et salle de bains ou douche. Un
           accès WIFI gratuit est accessible dans totues nos chambres et
@@ -27,8 +25,9 @@ const BoxBungalow = () => (
       <div className="col-12  bg-warning">
         <h5>Repas</h5>
         <p>
-          Petit Déjeuner : 1000 fr/Jour <br/>Demi-Pension : 2500 fr/Jour <br/>Pension
-          Complète: 4000 fr/Jour{' '}
+          Petit Déjeuner : 1000 fr/Jour <br />
+          Demi-Pension : 2500 fr/Jour <br />
+          Pension Complète: 4000 fr/Jour{' '}
         </p>
       </div>
       <div className="col-12  bg-warning">
@@ -36,6 +35,78 @@ const BoxBungalow = () => (
 
         <p>Garde Animaux : 2500 fr</p>
       </div>
+    </div>
+    <div className="container py-4">
+      <header className="pb-3 mb-4 border-bottom">
+        <a
+          href="/"
+          className="d-flex align-items-center text-dark text-decoration-none"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="32"
+            className="me-2"
+            viewBox="0 0 118 94"
+            role="img"
+          >
+            <title>Bootstrap</title>
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"
+              fill="currentColor"
+            ></path>
+          </svg>
+          <span className="fs-4">Jumbotron example</span>
+        </a>
+      </header>
+
+      <div className="p-5 mb-4 bg-light rounded-3">
+        <div className="container-fluid py-5">
+          <h1 className="display-5 fw-bold">Custom jumbotron</h1>
+          <p className="col-md-8 fs-4">
+            Using a series of utilities, you can create this jumbotron, just
+            like the one in previous versions of Bootstrap. Check out the
+            examples below for how you can remix and restyle it to your liking.
+          </p>
+          <button className="btn btn-primary btn-lg" type="button">
+            Example button
+          </button>
+        </div>
+      </div>
+
+      <div className="row align-items-md-stretch">
+        <div className="col-md-6">
+          <div className="h-100 p-5 text-bg-dark rounded-3">
+            <h2>Change the background</h2>
+            <p>
+              Swap the background-color utility and add a `.text-*` color
+              utility to mix up the jumbotron look. Then, mix and match with
+              additional component themes and more.
+            </p>
+            <button className="btn btn-outline-light" type="button">
+              Example button
+            </button>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="h-100 p-5 bg-light border rounded-3">
+            <h2>Add borders</h2>
+            <p>
+              Or, keep it light and add a border for some added definition to
+              the boundaries of your content. Be sure to look under the hood at
+              the source HTML here as we've adjusted the alignment and sizing of
+              both column's content for equal-height.
+            </p>
+            <button className="btn btn-outline-secondary" type="button">
+              Example button
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <footer className="pt-3 mt-4 text-muted border-top">&copy; 2022</footer>
     </div>
   </div>
 );
