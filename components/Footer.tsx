b5cae1944966c1cd50130ba1b92f1867f6e91750
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-28 14:48:36 -1000

    1 file modified

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,12 +7,20 @@ const Footer = () => {
           <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
             <h5 className="text-uppercase">Avis Clients</h5>
 
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
-              atque ea quis molestias. Fugiat pariatur maxime quis culpa
-              corporis vitae repudiandae aliquam voluptatem veniam, est atque
-              cumque eum delectus sint!
-            </p>
+            <div className="overflow-auto">...</div>
+            <div className="overflow-scroll">
+              Petit aperçu de la culture polynésienne avec le village et le
+              traditionnel "four Tahitien". Le repas est excellent ainsi que le
+              spectacle. Visité en juin 2014 nono1664{' '}
+            </div>
+            <div className="overflow-scroll">
+              Des animations sont prévus pour faire passer le temps entre les
+              plats. Enfin le spectacle, où la rien à dire, il faut vraiment le
+              voir!!! En résumé, le spectacle est vraiment à faire!!!! Visité en
+              juillet 2014 ggzergzezethzethzg e ngjznegj jerzg iezjrg ij oize
+              ozer okerzo jozjrg iezrgj ze jjzer j oreg ijzlkr jgzer nzge kn
+              zknrz lk kllzre rze rze kikrejjgikkjjh rhe e jzt hjjthz hjzeg
+            </div>
           </div>
 
           <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
@@ -21,7 +29,7 @@ const Footer = () => {
             <ul className="list-unstyled mb-0">
               <li>
                 <a href="#!" className="text-white">
-                  
+                  Plan du Site
                 </a>
               </li>
               <li>
@@ -38,8 +46,8 @@ const Footer = () => {
         className="text-center p-3"
         style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}
       >
-        © 2022 Copyright: Cet hôtel est la propriété de Beachcomber Lex Corp,
-        ©2022, LagoonPension est la
+        © 2022 Copyright: Cet hôtel est la propriété de Lex Corp, ©2022,
+        LagoonPension est la
         <a className="text-white" href="/">
           propriété de Lex
         </a>
@@ -48,16 +56,3 @@ const Footer = () => {
   );
 };
 export default Footer;
-
-CE QU’ILS DISENT
-Petit aperçu de la culture polynésienne avec le village et le traditionnel "four Tahitien". Le repas est excellent ainsi que le spectacle. Visité en juin 2014
-
-nono1664 - http://www.tripadvisor.fr
-
-Ayant déjà participé à un repas tahitien avec le four traditionnel nous sommes allés uniquement au spectacle et nous avons tous été emballés ! A ne pas manquer !
-
-Cédric D. - http://www.tripadvisor.fr
-
-Nous avions opté pour cette excursion au départ de notre hôtel. Après 40 min de route, nous sommes arrivés au Tikki Villlage mais l'activité avait déjà commencé! Du coup, l'hôtesse nous a vite donné le verre de bienvenu à l'entrée et nous a pressé vers la première scène: l'ouverture de la noix de coco. A peine assis, nous avons du rejoindre une autre scène: le four tahitien. Le guide vous présente votre repas cuit à la mode tahitienne: ce fut intéressant à voir.Ensuite la visite du village, assez vite expédiée. Puis vient le temps du repas: un buffet: un vrai régal. Des animations sont prévus pour faire passer le temps entre les plats. Enfin le spectacle, où la rien à dire, il faut vraiment le voir!!! En résumé, le spectacle est vraiment à faire!!!! Visité en juillet 2014
-
-Laure_2_Tise - http://www.tripadvisor.fr
