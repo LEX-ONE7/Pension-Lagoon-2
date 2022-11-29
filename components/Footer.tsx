7dbed8c43b5888b8bc8e6415626d98da6757f9d5
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-28 14:36:13 -1000

    1 file modified

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
       <div className="container p-4">
         <div className="row">
           <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-            <h5 className="text-uppercase">Footer Content</h5>
+            <h5 className="text-uppercase">Avis Clients</h5>
 
             <p>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste
@@ -21,7 +21,7 @@ const Footer = () => {
             <ul className="list-unstyled mb-0">
               <li>
                 <a href="#!" className="text-white">
-                  Avis Clients
+                  
                 </a>
               </li>
               <li>
@@ -41,10 +41,23 @@ const Footer = () => {
         © 2022 Copyright: Cet hôtel est la propriété de Beachcomber Lex Corp,
         ©2022, LagoonPension est la
         <a className="text-white" href="/">
-           propriété de Lex
+          propriété de Lex
         </a>
       </div>
     </div>
   );
 };
 export default Footer;
+
+CE QU’ILS DISENT
+Petit aperçu de la culture polynésienne avec le village et le traditionnel "four Tahitien". Le repas est excellent ainsi que le spectacle. Visité en juin 2014
+
+nono1664 - http://www.tripadvisor.fr
+
+Ayant déjà participé à un repas tahitien avec le four traditionnel nous sommes allés uniquement au spectacle et nous avons tous été emballés ! A ne pas manquer !
+
+Cédric D. - http://www.tripadvisor.fr
+
+Nous avions opté pour cette excursion au départ de notre hôtel. Après 40 min de route, nous sommes arrivés au Tikki Villlage mais l'activité avait déjà commencé! Du coup, l'hôtesse nous a vite donné le verre de bienvenu à l'entrée et nous a pressé vers la première scène: l'ouverture de la noix de coco. A peine assis, nous avons du rejoindre une autre scène: le four tahitien. Le guide vous présente votre repas cuit à la mode tahitienne: ce fut intéressant à voir.Ensuite la visite du village, assez vite expédiée. Puis vient le temps du repas: un buffet: un vrai régal. Des animations sont prévus pour faire passer le temps entre les plats. Enfin le spectacle, où la rien à dire, il faut vraiment le voir!!! En résumé, le spectacle est vraiment à faire!!!! Visité en juillet 2014
+
+Laure_2_Tise - http://www.tripadvisor.fr
