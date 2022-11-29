ad27d498dcf347737a6ca31a7e3c1a54bb776dec
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-28 19:16:02 -1000

    1 file modified

--- pages/Accueil.tsx
+++ pages/Accueil.tsx
@@ -97,31 +97,18 @@ const Accueil = () => {
       <div className="row featurette">
         <div className="col-md-7">
           <h2 className="featurette-heading fw-normal lh-1">
-            First featurette heading.{' '}
+            CHAMBRES ET BUNGALOWS.{' '}
             <span className="text-muted">It’ll blow your mind.</span>
           </h2>
           <p className="lead">
-            Some great placeholder content for the first featurette here.
-            Imagine some exciting prose here.
+            Au coeur des jardins tropicaux ou le long des eaux paisibles du
+            lagon, nos 10 chambres et bungalows sur pilotis vous offre confort
+            et authenticité polynésienne. Lumineux et spacieux, ce sont de
+            véritables havres de détente.
           </p>
         </div>
         <div className="col-md-5">
-          <svg
-            className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
-            width="500"
-            height="500"
-            xmlns="http://www.w3.org/2000/svg"
-            role="img"
-            aria-label="Placeholder: 500x500"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#eee" />
-            <text x="50%" y="50%" fill="#aaa" dy=".3em">
-              500x500
-            </text>
-          </svg>
+          <img src="https://www.tropicalement-votre.com/produits/1030/doc/chambre-vue-lagon.jpg" />{' '}
         </div>
       </div>
 
@@ -197,4 +184,3 @@ const Accueil = () => {
 };
 
 export default Accueil;
-
