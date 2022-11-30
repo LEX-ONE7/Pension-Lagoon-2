79330569b6f9cb59f1b3a683fa11c192322fee6a
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-11-29 14:40:55 -1000

    1 file modified

--- components/rooms/BoxBungalow.tsx
+++ components/rooms/BoxBungalow.tsx
@@ -9,17 +9,17 @@ const BoxBungalow = () => (
       </div>
     </div>
     <div className="container py-4">
-      <div className="p-5 mb-4 bg-white rounded-5">
+      <div className="p-5 mb-4 text-bg-info rounded-5">
         <div className="container-fluid py-5">
-          <h2 className="display-5 fw-bold">Bungalows</h2>
+          <h2 className="display-6 fw-bold">Bungalows</h2>
           <p className="col-md-12 fs-4">
             Les Bungalows Pilotis bénéficient d’une grande terrasse solarium
             privée et d’un accès direct au lagon.
           </p>
           <SliderAuto collection="layout-bungalows" />
-          <p>
+          <p className="fs-4 mt-4">
             Equipées d’un TV, d’un mini-bar, de WC et salle de bains ou douche.
-            Un accès WIFI gratuit est accessible dans totues nos chambres et
+            Un accès WIFI gratuit est accessible dans toutes nos chambres et
             bungalows. Hotel ouvert 7j/7
           </p>
 
@@ -33,12 +33,13 @@ const BoxBungalow = () => (
       </div>
       <div className="p-5 mb-4 bg-light rounded-5">
         <div className="container-fluid py-2">
-          <h2 className="display-5 fw-bold text-center">Chambres</h2>
-          <p className="col-md-8 fs-4">
+          <h2 className="display-6 fw-bold text-center float-end mb-4">Chambres</h2>
+          <p className=" fs-4">
             Quelles que soient leur catégorie et leur situation, toutes nos
             chambres vous offrira un concentré de confort.
           </p>
           <SliderAuto collection="Chambres" />
+          
           <button
             className="btn btn-info btn-lg rounded-3 btn-outline-light mt-4"
             type="button"
@@ -57,9 +58,9 @@ const BoxBungalow = () => (
               src="https://www.yestahiti.fr/_client_files/infos_voyageurs/chaud-froid.jpg"
             />
             <h2 className="mt-4 mb-4">Chaud-Froid</h2>
-            <p>Tataki au thon rouge.</p>
+            <p className="fs-5">Tataki au thon rouge.</p>
 
-            <button className="btn btn-outline-light" type="button">
+            <button className="btn btn-outline-light mt-4" type="button">
               Commander
             </button>
           </div>
@@ -72,8 +73,8 @@ const BoxBungalow = () => (
               src="https://img.theculturetrip.com/1440x/smart/wp-content/uploads/2020/02/feet5t.jpg"
             />
             <h2 className="mt-4 mb-4">Crudités</h2>
-            <p>Poisson cru au lait de coco.</p>
-            <button className="btn btn-outline-light" type="button">
+            <p className="fs-5">Poisson cru au lait de coco.</p>
+            <button className="btn btn-outline-light mt-4" type="button">
               Commander
             </button>
           </div>
@@ -88,7 +89,7 @@ const BoxBungalow = () => (
             Demi-Pension : 2500 fr/Jour <br />
             Pension Complète: 4000 fr/Jour{' '}
           </p>
-        
+
           <div className="col-12  bg-warning">
             <h6 className="display-5 fw-bold">Autres</h6>
 
