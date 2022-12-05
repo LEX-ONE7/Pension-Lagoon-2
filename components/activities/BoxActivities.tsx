b8e1755ac660144d04f62df98bc6f6b65a5f8c17
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-04 20:25:39 -1000

    2 files modified

--- components/activities/BoxActivities.tsx
+++ components/activities/BoxActivities.tsx
@@ -3,30 +3,6 @@ const BoxActivities = () => (
   <div className="container-fluid">
     <div className="row">
       <div className="col-12 col-md-8 bg-success">
-        <h3> Nautiques</h3>
-
-        <p>
-          {' '}
-          Entouré par les eaux cristallines de l'océan Pacifique Sud,
-          l'InterContinental Tahiti Resort & Spa propose une mutitude de sports
-          nautiques. Offrez-vous une excursion guidée de plongée en apnée ou une
-          sortie en voilier, partez à l'aventure pour surfer les vagues de
-          Tahiti ou faites le plein de sensations aquatiques en kayak ou en jet
-          ski.{' '}
-        </p>
-      </div>
-      <div className="col-12 col-md-8 bg-info">
-        <h4>Terrestres</h4>
-
-        <p>
-          {' '}
-          Tahiti est une île regorgeant de trésors cachés, de ses vallées
-          verdoyantes à ses lagons scintillants. Pour découvrir l'île sous tous
-          ses aspects, nous proposons des excursions tous terrains, pour tous
-          les niveaux de forme physique. Faites le tour de l'île accompagné d'un
-          guide, explorez Tahiti en 4x4, partez à l'aventure hors des sentiers
-          battus ou en promenade sur les chemins balisés à travers la jungle.{' '}
-        </p>
         <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
           <div className="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
             <div className="my-3 py-3">
@@ -35,88 +11,130 @@ const BoxActivities = () => (
             </div>
             <div
               className="bg-light shadow-sm mx-auto"
-              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
+              style={{
+                width: '80%',
+                height: '300px',
+                bordeRadius: '21px 21px 0 0 ',
+              }}
             ></div>
-          </div>
-          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 p-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
+            <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+              <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div className="my-3 p-3">
+                  <h2 className="display-5">Another headline</h2>
+                  <p className="lead">And an even wittier subheading.</p>
+                </div>
+                <div
+                  className="bg-dark shadow-sm mx-auto"
+                  style={{
+                    width: '80%',
+                    height: '300px',
+                    bordeRadius: '21px 21px 0 0 ',
+                  }}
+                ></div>
+              </div>
+              <div className="text-bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div className="my-3 py-3">
+                  <h2 className="display-5">Another headline</h2>
+                  <p className="lead">And an even wittier subheading.</p>
+                </div>
+                <div
+                  className="bg-light shadow-sm mx-auto"
+                  style={{
+                    width: '80%',
+                    height: '300px',
+                    bordeRadius: '21px 21px 0 0 ',
+                  }}
+                ></div>
+              </div>
             </div>
-            <div
-              className="bg-dark shadow-sm mx-auto"
-              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-            ></div>
-          </div>
-        </div>
 
-        <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 p-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
+            <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+              <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div className="my-3 p-3">
+                  <h2 className="display-5">Another headline</h2>
+                  <p className="lead">And an even wittier subheading.</p>
+                </div>
+                <div
+                  className="bg-body shadow-sm mx-auto"
+                  style={{
+                    width: '80%',
+                    height: '300px',
+                    bordeRadius: '21px 21px 0 0 ',
+                  }}
+                ></div>
+              </div>
+              <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div className="my-3 py-3">
+                  <h2 className="display-5">Another headline</h2>
+                  <p className="lead">And an even wittier subheading.</p>
+                </div>
+                <div
+                  className="bg-body shadow-sm mx-auto"
+                  style={{
+                    width: '80%',
+                    height: '300px',
+                    bordeRadius: '21px 21px 0 0 ',
+                  }}
+                ></div>
+              </div>
             </div>
-            <div
-              className="bg-dark shadow-sm mx-auto"
-              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-            ></div>
-          </div>
-          <div className="text-bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 py-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-            <div
-              className="bg-light shadow-sm mx-auto"
-              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-            ></div>
-          </div>
-        </div>
 
-        <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 p-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
+            <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+              <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div className="my-3 p-3">
+                  <h2 className="display-5">Another headline</h2>
+                  <p className="lead">And an even wittier subheading.</p>
+                </div>
+                <div
+                  className="bg-body shadow-sm mx-auto"
+                  style={{
+                    width: '80%',
+                    height: '300px',
+                    bordeRadius: '21px 21px 0 0 ',
+                  }}
+                ></div>
+              </div>
+              <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+                <div className="my-3 py-3">
+                  <h2 className="display-5">Another headline</h2>
+                  <p className="lead">And an even wittier subheading.</p>
+                </div>
+                <div
+                  className="bg-body shadow-sm mx-auto"
+                  style={{
+                    width: '80%',
+                    height: '300px',
+                    bordeRadius: '21px 21px 0 0 ',
+                  }}
+                ></div>
+              </div>
             </div>
-            <div
-              className="bg-body shadow-sm mx-auto"
-              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-            ></div>
           </div>
-          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 py-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-            <div
-              className="bg-body shadow-sm mx-auto"
-              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-            ></div>
-          </div>
-        </div>
+          <h3> Nautiques</h3>
 
-        <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 p-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-            <div
-              className="bg-body shadow-sm mx-auto"
-              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-            ></div>
-          </div>
-          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div className="my-3 py-3">
-              <h2 className="display-5">Another headline</h2>
-              <p className="lead">And an even wittier subheading.</p>
-            </div>
-            <div
-              className="bg-body shadow-sm mx-auto"
-              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
-            ></div>
-          </div>
+          <p>
+            {' '}
+            Entouré par les eaux cristallines de l'océan Pacifique Sud,
+            l'InterContinental Tahiti Resort & Spa propose une mutitude de
+            sports nautiques. Offrez-vous une excursion guidée de plongée en
+            apnée ou une sortie en voilier, partez à l'aventure pour surfer les
+            vagues de Tahiti ou faites le plein de sensations aquatiques en
+            kayak ou en jet ski.{' '}
+          </p>
+        </div>
+        <div className="col-12 col-md-8 bg-info">
+          <h4>Terrestres</h4>
+
+          <p>
+            {' '}
+            Tahiti est une île regorgeant de trésors cachés, de ses vallées
+            verdoyantes à ses lagons scintillants. Pour découvrir l'île sous
+            tous ses aspects, nous proposons des excursions tous terrains, pour
+            tous les niveaux de forme physique. Faites le tour de l'île
+            accompagné d'un guide, explorez Tahiti en 4x4, partez à l'aventure
+            hors des sentiers battus ou en promenade sur les chemins balisés à
+            travers la jungle.{' '}
+          </p>
         </div>
       </div>
     </div>
