27c50f6e0e65072455e78f3f93f574a992dff4f2
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-04 21:41:46 -1000

    1 file modified

--- components/rooms/BoxBungalow.tsx
+++ components/rooms/BoxBungalow.tsx
@@ -62,8 +62,6 @@ const BoxBungalow = () => (
             />
             <h2 className="mt-4 mb-4">Chaud-Froid</h2>
             <p className="fs-5">Tataki au thon rouge.</p>
-
-          
           </div>
         </div>
         <div className="col-md-6">
@@ -75,80 +73,66 @@ const BoxBungalow = () => (
             />
             <h2 className="mt-4 mb-4">Crudités</h2>
             <p className="fs-5">Poisson cru au lait de coco.</p>
-          
           </div>
         </div>
       </div>
-      <div className="p-5 mt-4 mb-4 bg-whitesmoke rounded-6">
-        <div className="row space-around center">
-          <div className="col">
-            <div className="card mb-4 rounded-3 shadow-sm">
-              <div className="card-header py-3">
-                <h4 className="my-0 fw-normal"> Petit Déjeuner</h4>
-              </div>
-              <div className="card-body">
-                <h1 className="card-title pricing-card-title">
-                  1000 fr<small className="text-muted fw-light">/Jour</small>
-                </h1>
-                <ul className="list-unstyled mt-3 mb-4">
-                  <li>Prix/pers</li>
-                </ul>
-                <button
-                  type="button"
-                  className="w-100 btn btn-lg btn-info btn-lg rounded-3 btn-outline-light"
-                >
-                  Réserver une table
-                </button>
-                
-              </div>
+      <div className="pt-5 mt-4 mb-4 bg-whitesmoke rounded-3">
+        <div className="row space-between  row-cols-lg-3 text-center">
+          <div className="card  shadow-sm">
+            <div className="card-header py-3">
+              <h4 className="my-0 fw-normal"> Petit Déjeuner</h4>
             </div>
-            <div className="card mb-4 rounded-3 shadow-sm">
-              <div className="card-header py-3">
-                <h4 className="my-0 fw-normal"> Demi-Pension</h4>
-              </div>
-              <div className="card-body">
-                <h1 className="card-title pricing-card-title">
-                  2500 fr<small className="text-muted fw-light">/Jour</small>
-                </h1>
-                <ul className="list-unstyled mt-3 mb-4">
-                  <li>Prix/pers</li>
-                </ul>
-                <button
-                  type="button"
-                  className="w-100 btn btn-lg btn-info btn-lg rounded-3 btn-outline-light"
-                >
-                  Réserver une table
-                </button>
-                
-              </div>
-            </div>
-            <div className="card mb-4 rounded-3 shadow-sm">
-              <div className="card-header py-3">
-                <h4 className="my-0 fw-normal"> Pension Complète</h4>
-              </div>
-              <div className="card-body">
-                <h1 className="card-title pricing-card-title">
-                  4000 fr<small className="text-muted fw-light">/Jour</small>
-                </h1>
-                <ul className="list-unstyled mt-3 mb-4">
-                  <li>Prix/pers</li>
-                </ul>
-                <button
-                  type="button"
-                  className="w-100 btn btn-lg btn-info btn-lg rounded-3 btn-outline-light"
-                >
-                  Réserver une table
-                </button>
-                
-              </div>
+            <div className="card-body">
+              <h1 className="card-title pricing-card-title">
+                1000 fr<small className="text-muted fw-light">/Jour</small>
+              </h1>
+              <ul className="list-unstyled mt-3 mb-4">
+                <li>Prix/pers</li>
+              </ul>
+              <button
+                type="button"
+                className="w-100 btn btn-lg btn-info btn-lg rounded-3 btn-outline-light"
+              >
+                Réserver une table
+              </button>
             </div>
           </div>
-          <div className="container-fluid py-2">
-            
-            <div className="col-12  bg-warning">
-              <h6 className="display-5 fw-bold">Autres</h6>
-
-              <p>Garde Animaux : 2500 fr</p>
+          <div className="card  shadow-sm">
+            <div className="card-header py-3">
+              <h4 className="my-0 fw-normal"> Demi-Pension</h4>
+            </div>
+            <div className="card-body">
+              <h1 className="card-title pricing-card-title">
+                2500 fr<small className="text-muted fw-light">/Jour</small>
+              </h1>
+              <ul className="list-unstyled mt-3 mb-4">
+                <li>Prix/pers</li>
+              </ul>
+              <button
+                type="button"
+                className="w-100 btn btn-lg btn-info btn-lg rounded-3 btn-outline-light"
+              >
+                Réserver une table
+              </button>
+            </div>
+          </div>
+          <div className="card  shadow-sm">
+            <div className="card-header py-3">
+              <h4 className="my-0 fw-normal"> Pension Complète</h4>
+            </div>
+            <div className="card-body">
+              <h1 className="card-title pricing-card-title">
+                4000 fr<small className="text-muted fw-light">/Jour</small>
+              </h1>
+              <ul className="list-unstyled mt-3 mb-4">
+                <li>Prix/pers</li>
+              </ul>
+              <button
+                type="button"
+                className="w-100 btn btn-lg btn-info btn-lg rounded-3 btn-outline-light"
+              >
+                Réserver une table
+              </button>
             </div>
           </div>
         </div>
