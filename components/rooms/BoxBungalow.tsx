09d896625228fb0cff7ac331b1ce0f0a888ac06b
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-05 10:08:13 -1000

    1 file modified

--- components/rooms/BoxBungalow.tsx
+++ components/rooms/BoxBungalow.tsx
@@ -49,100 +49,97 @@ const BoxBungalow = () => (
           >
             Réserver
           </button>
+          <p className="float-end">Garde Animaux : 2500 fr</p>
+        </div>
+      </div>
+    </div>
+
+    <div className="row align-items-md-stretch">
+      <div className="col-md-6">
+        <div className="h-100 p-5 text-bg-dark rounded-3 text-center">
+          <img
+            style={{ height: '450px' }}
+            className="img-fluid w-100"
+            src="https://www.yestahiti.fr/_client_files/infos_voyageurs/chaud-froid.jpg"
+          />
+          <h2 className="mt-4 mb-4">Chaud-Froid</h2>
+          <p className="fs-5">Tataki au thon rouge.</p>
+        </div>
+      </div>
+      <div className="col-md-6">
+        <div className="h-100 p-5 text-bg-dark border rounded-3 text-center">
+          <img
+            style={{ height: '450px' }}
+            className="img-fluid w-100"
+            src="https://img.theculturetrip.com/1440x/smart/wp-content/uploads/2020/02/feet5t.jpg"
+          />
+          <h2 className="mt-4 mb-4">Crudités</h2>
+          <p className="fs-5">Poisson cru au lait de coco.</p>
+        </div>
+      </div>
+    </div>
+    <div className="pt-5 mt-4 mb-4 bg-whitesmoke rounded-3">
+      <div className="row   row-cols-lg-3 space-between text-center">
+        <div className="card  shadow-sm">
+          <div className="card-header py-3">
+            <h4 className="my-0 fw-normal"> Petit Déjeuner</h4>
+          </div>
+          <div className="card-body">
+            <h1 className="card-title pricing-card-title">
+              1000 fr<small className="text-muted fw-light">/Jour</small>
+            </h1>
+            <ul className="list-unstyled mt-3 mb-4">
+              <li>Prix/pers</li>
+            </ul>
+            <button
+              type="button"
+              className="w-100 btn btn-lg btn-info btn-lg rounded-3 btn-outline-light"
+            >
+              Réserver une table
+            </button>
+          </div>
+        </div>
+        <div className="card  shadow-sm">
+          <div className="card-header py-3">
+            <h4 className="my-0 fw-normal"> Demi-Pension</h4>
+          </div>
+          <div className="card-body">
+            <h1 className="card-title pricing-card-title">
+              2500 fr<small className="text-muted fw-light">/Jour</small>
+            </h1>
+            <ul className="list-unstyled mt-3 mb-4">
+              <li>Prix/pers</li>
+            </ul>
+            <button
+              type="button"
+              className="w-100 btn btn-lg btn-info btn-lg rounded-3 btn-outline-light"
+            >
+              Réserver une table
+            </button>
+          </div>
+        </div>
+        <div className="card  shadow-sm">
+          <div className="card-header py-3">
+            <h4 className="my-0 fw-normal"> Pension Complète</h4>
+          </div>
+          <div className="card-body">
+            <h1 className="card-title pricing-card-title">
+              4000 fr<small className="text-muted fw-light">/Jour</small>
+            </h1>
+            <ul className="list-unstyled mt-3 mb-4">
+              <li>Prix/pers</li>
+            </ul>
+            <button
+              type="button"
+              className="w-100 btn btn-lg btn-info btn-lg rounded-3 btn-outline-light"
+            >
+              Réserver une table
+            </button>
+          </div>
         </div>
       </div>
 
-      <div className="row align-items-md-stretch">
-        <div className="col-md-6">
-          <div className="h-100 p-5 text-bg-dark rounded-3 text-center">
-            <img
-              style={{ height: '450px' }}
-              className="img-fluid w-100"
-              src="https://www.yestahiti.fr/_client_files/infos_voyageurs/chaud-froid.jpg"
-            />
-            <h2 className="mt-4 mb-4">Chaud-Froid</h2>
-            <p className="fs-5">Tataki au thon rouge.</p>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <div className="h-100 p-5 text-bg-dark border rounded-3 text-center">
-            <img
-              style={{ height: '450px' }}
-              className="img-fluid w-100"
-              src="https://img.theculturetrip.com/1440x/smart/wp-content/uploads/2020/02/feet5t.jpg"
-            />
-            <h2 className="mt-4 mb-4">Crudités</h2>
-            <p className="fs-5">Poisson cru au lait de coco.</p>
-          </div>
-        </div>
-      </div>
-      <div className="pt-5 mt-4 mb-4 bg-whitesmoke rounded-3">
-        <div className="row   row-cols-lg-3 space-between text-center">
-          <div className="card  shadow-sm">
-            <div className="card-header py-3">
-              <h4 className="my-0 fw-normal"> Petit Déjeuner</h4>
-            </div>
-            <div className="card-body">
-              <h1 className="card-title pricing-card-title">
-                1000 fr<small className="text-muted fw-light">/Jour</small>
-              </h1>
-              <ul className="list-unstyled mt-3 mb-4">
-                <li>Prix/pers</li>
-              </ul>
-              <button
-                type="button"
-                className="w-100 btn btn-lg btn-info btn-lg rounded-3 btn-outline-light"
-              >
-                Réserver une table
-              </button>
-            </div>
-          </div>
-          <div className="card  shadow-sm">
-            <div className="card-header py-3">
-              <h4 className="my-0 fw-normal"> Demi-Pension</h4>
-            </div>
-            <div className="card-body">
-              <h1 className="card-title pricing-card-title">
-                2500 fr<small className="text-muted fw-light">/Jour</small>
-              </h1>
-              <ul className="list-unstyled mt-3 mb-4">
-                <li>Prix/pers</li>
-              </ul>
-              <button
-                type="button"
-                className="w-100 btn btn-lg btn-info btn-lg rounded-3 btn-outline-light"
-              >
-                Réserver une table
-              </button>
-            </div>
-          </div>
-          <div className="card  shadow-sm">
-            <div className="card-header py-3">
-              <h4 className="my-0 fw-normal"> Pension Complète</h4>
-            </div>
-            <div className="card-body">
-              <h1 className="card-title pricing-card-title">
-                4000 fr<small className="text-muted fw-light">/Jour</small>
-              </h1>
-              <ul className="list-unstyled mt-3 mb-4">
-                <li>Prix/pers</li>
-              </ul>
-              <button
-                type="button"
-                className="w-100 btn btn-lg btn-info btn-lg rounded-3 btn-outline-light"
-              >
-                Réserver une table
-              </button>
-            </div>
-          </div>
-          <div className=" bg-whitesmoke float-end">
-            <h6 className="display-6  ">Autres</h6>
-
-            <p>Garde Animaux : 2500 fr</p>
-          </div>
-        </div>
-        <footer className="pt-3 mt-4 text-muted border-top">&copy; 2022</footer>
-      </div>
+      <footer className="pt-3 mt-4 text-muted border-top">&copy; 2022</footer>
     </div>
   </div>
 );
