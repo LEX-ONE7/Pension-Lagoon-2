42c7b20aa9d503960ea210725be4aad82331ca01
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-04 19:25:12 -1000

    1 file modified

--- components/activities/BoxActivities.tsx
+++ components/activities/BoxActivities.tsx
@@ -27,6 +27,97 @@ const BoxActivities = () => (
           guide, explorez Tahiti en 4x4, partez à l'aventure hors des sentiers
           battus ou en promenade sur les chemins balisés à travers la jungle.{' '}
         </p>
+        <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+          <div className="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div className="my-3 py-3">
+              <h2 className="display-5">Another headline</h2>
+              <p className="lead">And an even wittier subheading.</p>
+            </div>
+            <div
+              className="bg-light shadow-sm mx-auto"
+              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
+            ></div>
+          </div>
+          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div className="my-3 p-3">
+              <h2 className="display-5">Another headline</h2>
+              <p className="lead">And an even wittier subheading.</p>
+            </div>
+            <div
+              className="bg-dark shadow-sm mx-auto"
+              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
+            ></div>
+          </div>
+        </div>
+
+        <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div className="my-3 p-3">
+              <h2 className="display-5">Another headline</h2>
+              <p className="lead">And an even wittier subheading.</p>
+            </div>
+            <div
+              className="bg-dark shadow-sm mx-auto"
+              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
+            ></div>
+          </div>
+          <div className="text-bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div className="my-3 py-3">
+              <h2 className="display-5">Another headline</h2>
+              <p className="lead">And an even wittier subheading.</p>
+            </div>
+            <div
+              className="bg-light shadow-sm mx-auto"
+              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
+            ></div>
+          </div>
+        </div>
+
+        <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div className="my-3 p-3">
+              <h2 className="display-5">Another headline</h2>
+              <p className="lead">And an even wittier subheading.</p>
+            </div>
+            <div
+              className="bg-body shadow-sm mx-auto"
+              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
+            ></div>
+          </div>
+          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div className="my-3 py-3">
+              <h2 className="display-5">Another headline</h2>
+              <p className="lead">And an even wittier subheading.</p>
+            </div>
+            <div
+              className="bg-body shadow-sm mx-auto"
+              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
+            ></div>
+          </div>
+        </div>
+
+        <div className="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div className="my-3 p-3">
+              <h2 className="display-5">Another headline</h2>
+              <p className="lead">And an even wittier subheading.</p>
+            </div>
+            <div
+              className="bg-body shadow-sm mx-auto"
+              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
+            ></div>
+          </div>
+          <div className="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div className="my-3 py-3">
+              <h2 className="display-5">Another headline</h2>
+              <p className="lead">And an even wittier subheading.</p>
+            </div>
+            <div
+              className="bg-body shadow-sm mx-auto"
+              style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"
+            ></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
