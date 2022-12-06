11cba6ac054aab007d616bb42abb14dd7d89e957
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-05 14:15:11 -1000

    1 file modified

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     <div className="bg-secondary text-white text-center text-lg-start">
       <div className="container p-4">
         <div className="row">
-          <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+          <div className="col-lg-6 mb-4 mb-md-0">
             <h5 className="text-uppercase">Avis Clients</h5>
 
             <div
@@ -36,7 +36,7 @@ const Footer = () => {
             </div>
           </div>
 
-          <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <div className="col-lg-6 col-md-6 mb-4 mb-md-0">
             <h5 className="text-uppercase">Links</h5>
 
             <ul className="list-unstyled mb-0">
