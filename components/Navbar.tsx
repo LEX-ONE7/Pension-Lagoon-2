85feae0fa3d24f1b7a6185591ec443f5b812bd26
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-05 19:00:43 -1000

    1 file modified

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => (
         
         
         "
-          className="rounded-circle "
+          className="rounded-circle mx-5"
         />
         <p>LAGOON LODGE</p>
       </a>
