f68aa7cc58aedc5466c4b07c2c4397eb2df55904
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-06 10:49:20 -1000

    1 file modified

--- components/rooms/BoxBungalow.tsx
+++ components/rooms/BoxBungalow.tsx
@@ -19,11 +19,7 @@ const BoxBungalow = () => (
           <SliderAuto collection="layout-bungalows" />
           <p className="float-end">Garde Animaux : 2500 fr/Jour</p>
 
-          <p className="fs-4 mt-4 text-white">
-            Equipées d’un TV, d’un mini-bar, de WC et salle de bains ou douche.
-            Un accès WIFI gratuit est accessible dans toutes nos chambres et
-            bungalows.
-          </p>
+          <p className="fs-4 mt-4 text-white"></p>
 
           <button
             className="btn btn-lg rounded-3 btn-outline-light mt-4"
@@ -36,12 +32,13 @@ const BoxBungalow = () => (
 
       <div className="p-5 mb-4 bg-light rounded-5">
         <div className="container-fluid">
-          <h2 className="display-6 fw-bold float-end ">Chambres</h2>
+          <h2 className="display-6 fw-bold">Chambres</h2>
           <p className=" col-md-12 fs-4">
             Quelles que soient leur catégorie et leur situation, toutes nos
             chambres vous offrira un concentré de confort.
           </p>
           <SliderAuto collection="Chambres" />
+          <p className="float-end">Garde Animaux : 2500 fr/Jour</p>
 
           <button
             className="btn btn-info btn-lg rounded-3 btn-outline-light mt-4"
@@ -49,7 +46,6 @@ const BoxBungalow = () => (
           >
             Réserver
           </button>
-          <p className="float-end">Garde Animaux : 2500 fr/Jour</p>
         </div>
       </div>
     </div>
