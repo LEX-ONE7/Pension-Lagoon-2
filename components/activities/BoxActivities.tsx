99686ff1c1dae8cc2c3488344fa1cd9da31bcbfe
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-07 13:53:42 -1000

    2 files modified

--- components/activities/BoxActivities.tsx
+++ components/activities/BoxActivities.tsx
@@ -71,3 +71,20 @@ const BoxActivities = () => (
   </div>
 );
 export default BoxActivities;
+
+function rgba(arg0: number, arg1: number, arg2: number, arg3: number): any {
+  throw new Error('Function not implemented.');
+}
+
+function deg(
+  arg0: number,
+  deg: any,
+  arg2: any,
+  arg3: number,
+  arg4: number,
+  arg5: number,
+  arg6: number,
+  arg7: number
+) {
+  throw new Error('Function not implemented.');
+}
