2ee0b1d3baf37cd9eb8d482fe8f5bb34be5964ab
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-07 13:10:47 -1000

    1 file modified

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 const Navbar = () => (
-  <nav className="navbar navbar-expand-lg navbar-dark bg-info ">
+  <nav className="navbar navbar-expand-lg navbar-dark">
     <div className="container-fluid ">
       <a className="navbar-brand " href="#">
         <img
@@ -26,7 +26,7 @@ const Navbar = () => (
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse float-end" id="navbarNav">
+      <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav fs-5">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">
