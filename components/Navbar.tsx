92df7a7439a50f1073a4469408a0acac12cf1452
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-07 09:10:10 -1000

    1 file modified

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -27,7 +27,7 @@ const Navbar = () => (
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse " id="navbarNav">
-        <ul className="navbar-nav fs-5">
+        <ul className="navbar-nav fs-5 float-end">
           <li className="nav-item">
             <a className="nav-link active" aria-current="page" href="#">
               Accueil
