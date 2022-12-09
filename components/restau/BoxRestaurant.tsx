3c2388bdd5c50e575b8447dbe2613afdd3365d2d
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-08 14:02:42 -1000

    1 file modified

--- components/restau/BoxRestaurant.tsx
+++ components/restau/BoxRestaurant.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 const BoxRestaurant = () => {
- 
+  <div className="container-fluid">
+    <div className="row"> </div>
+  </div>;
 };
 export default BoxRestaurant;
