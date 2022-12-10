73509eb61ae26ebbff696a2c5f5f7d6b45e4ca3a
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-09 17:38:29 -1000

    1 file modified

--- components/Parallax.tsx
+++ components/Parallax.tsx
@@ -1,19 +1,13 @@
 import * as React from 'react';
 import { Parallax, Background } from 'react-parallax';
 
-const Parallax = () => {
-  return (
-    <Parallax
-      blur={{ min: -15, max: 15 }}
-      bgImage={{
-        src: 'https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D',
-      }}
-      bgImageAlt="the dog"
-      strength={-200}
-    >
-      Blur transition from min to max
-      <div style={{ height: '200px' }} />
+const Parallax = () => (
+  <div style={styles}>
+    <Parallax bgImage={image1} strength={500}>
+      <div style={{ height: 500 }}>
+        <div style={insideStyles}>HTML inside the parallax</div>
+      </div>
     </Parallax>
-  );
-};
+  </div>
+);
 export default Parallax;
