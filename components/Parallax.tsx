c558c9e3367a9cd8ba1ca558e6244b111eba8549
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-09 19:25:27 -1000

    1 file modified

--- components/Parallax.tsx
+++ components/Parallax.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import  {Parallax  } from 'react-parallax';
+import { Parallax } from 'react-parallax';
 
 const ImageOne = () => (
-      <Parallax
-        bgImage={`url("https://via.placeholder.com/500")` }
-        strength={500}
-      >
-        Content goes here. Parallax height grows with content height.
-      </Parallax>
-  );
+  <Parallax bgImage={`url("https://via.placeholder.com/500")`} strength={500}>
+    <div className="content">
+      <span className="img-txt">A trip for space</span>
+    </div>
+  </Parallax>
+);
 export default ImageOne;
