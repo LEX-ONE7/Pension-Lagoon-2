154cafe01354e836623a9ee1db04c88a2ab99d8a
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-09 20:13:45 -1000

    3 files modified

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
-import ImageOne from './components/Parallax';
 import SliderAuto from './components/SliderAuto';
 import Accueil from './pages/Accueil';
 import './style.css';
@@ -10,7 +9,6 @@ export default function App() {
   return (
     <div>
       <Navbar />
-      <ImageOne />
       <SliderAuto collection="header-carrousel" />
       <Accueil />
       <Footer />
--- components/Parallax.tsx
+++ components/Parallax.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { Parallax } from 'react-parallax';
 const image1 =
   'https://images.unsplash.com/photo-1532408840957-031d8034aeef?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80';
-const ImageOne = () => (
+const Para = () => (
   <Parallax className="image" bgImage={image1} strength={800}>
     <div className="content">
-      <span className="img-txt">A trip for space</span>
+      <span className="img-txt">BUNGALOWS</span>
     </div>
   </Parallax>
 );
-export default ImageOne;
+export default Para;
