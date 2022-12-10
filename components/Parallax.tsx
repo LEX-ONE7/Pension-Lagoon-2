d2d34a5f5b8ccbb43b14283ddde5bf0d04210fcb
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-09 19:15:19 -1000

    1 file modified

--- components/Parallax.tsx
+++ components/Parallax.tsx
@@ -1,19 +1,8 @@
 import * as React from 'react';
 
 const Parallax = () => {
-  return (
-    <div id="imPageRow_2" className="imPageRow imParallax">
-      <div style={{position: "absolute; top: 0; left: 0; right: 0; bottom: 0", overflow: "hidden",}}>
-        <div
-          id="imPageRowGraphics_2"
-          className="imParallaxBackground"
-          data-parallax-direction="down"
-          data-parallax-zoom="140"
-        >
-          <img src="https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D"/>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>
+    
+  </div>;
 };
 export default Parallax;
