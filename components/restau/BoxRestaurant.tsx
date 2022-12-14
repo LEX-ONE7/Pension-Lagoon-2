e8bb71c0bf93885e184b02faf00a8d552d051b94
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-13 22:47:15 -1000

    1 file modified

--- components/restau/BoxRestaurant.tsx
+++ components/restau/BoxRestaurant.tsx
@@ -4,22 +4,7 @@ const BoxRestaurant = () => {
     <div className="container-fluid">
       <div className="row">
         {/* Activités */}
-        <div className="jumbotron">
-          <div className="container">
-            <h1 className="display-3">Hello, world!</h1>
-            <p>
-              This is a template for a simple marketing or informational
-              website. It includes a large callout called a jumbotron and three
-              supporting pieces of content. Use it as a starting point to create
-              something more unique.
-            </p>
-            <p>
-              <a className="btn btn-primary btn-lg" href="#" role="button">
-                Learn more &raquo;
-              </a>
-            </p>
-          </div>
-        </div>
+        
 
         <div className="container">
           <div className="row p-2">
@@ -42,43 +27,6 @@ const BoxRestaurant = () => {
               >
                 Réserver une table
               </button>
-
-              <div className="col-md-4 text-center card rounded-5">
-                <h2>Demi Pension</h2>
-                <img
-                  src="https://7.holiday/public/user_files/1/1/7_villas.jpg"
-                  className="card-img"
-                />
-                <div className="card-img-text">
-                  <h3 className="card-title text-center fs-3 fw-bold align-content-center">
-                    Évènements
-                  </h3>
-                  <p className="text-dark lead fs-4 text-center">
-                    Vivez une expérience inoubliable !
-                  </p>
-                </div>
-                <a className="btn btn-secondary" href="" role="button">
-                  View details &raquo;
-                </a>
-              </div>
-              <div className="col-md-4 text-center card rounded-5">
-                <h2>Pension Complète</h2>
-                <img
-                  src="https://7.holiday/public/user_files/1/1/7_villas.jpg"
-                  className="card-img"
-                />
-                <div className="card-img-text">
-                  <h3 className="card-title text-center fs-3 fw-bold align-content-center">
-                    Évènements
-                  </h3>
-                  <p className="text-dark lead fs-4 text-center">
-                    Vivez une expérience inoubliable !
-                  </p>
-                </div>
-                <a className="btn btn-secondary" href="" role="button">
-                  View details &raquo;
-                </a>
-              </div>
             </div>
           </div>
         </div>
