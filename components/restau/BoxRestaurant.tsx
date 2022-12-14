a7b07be085838ba9a52ab759b3c618e9d2f54d8e
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-13 22:21:45 -1000

    1 file modified

--- components/restau/BoxRestaurant.tsx
+++ components/restau/BoxRestaurant.tsx
@@ -22,7 +22,7 @@ const BoxRestaurant = () => {
         </div>
 
         <div className="container">
-          <div className="row">
+          <div className="row px-2">
             <div className="col-md-4 text-center card rounded-5">
               <h2>Petit Dejeuner</h2>
               <img
@@ -37,37 +37,45 @@ const BoxRestaurant = () => {
                   Vivez une expérience inoubliable !
                 </p>
               </div>
-              <a className="btn btn-secondary" href="#" role="button">
+              <a className="btn btn-secondary" href="" role="button">
                 View details &raquo;
               </a>
             </div>
-            <div className="col-md-4">
-              <h2>Heading</h2>
-              <p>
-                Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
-                tellus ac cursus commodo, tortor mauris condimentum nibh, ut
-                fermentum massa justo sit amet risus. Etiam porta sem malesuada
-                magna mollis euismod. Donec sed odio dui.{' '}
-              </p>
-              <p>
-                <a className="btn btn-secondary" href="#" role="button">
-                  View details &raquo;
-                </a>
-              </p>
+            <div className="col-md-4 text-center card rounded-5">
+              <h2>Demi Pension</h2>
+              <img
+                src="https://7.holiday/public/user_files/1/1/7_villas.jpg"
+                className="card-img"
+              />
+              <div className="card-img-text">
+                <h3 className="card-title text-center fs-3 fw-bold align-content-center">
+                  Évènements
+                </h3>
+                <p className="text-dark lead fs-4 text-center">
+                  Vivez une expérience inoubliable !
+                </p>
+              </div>
+              <a className="btn btn-secondary" href="" role="button">
+                View details &raquo;
+              </a>
             </div>
-            <div className="col-md-4">
-              <h2>Heading</h2>
-              <p>
-                Donec sed odio dui. Cras justo odio, dapibus ac facilisis in,
-                egestas eget quam. Vestibulum id ligula porta felis euismod
-                semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris
-                condimentum nibh, ut fermentum massa justo sit amet risus.
-              </p>
-              <p>
-                <a className="btn btn-secondary" href="#" role="button">
-                  View details &raquo;
-                </a>
-              </p>
+            <div className="col-md-4 text-center card rounded-5">
+              <h2>Pension Complète</h2>
+              <img
+                src="https://7.holiday/public/user_files/1/1/7_villas.jpg"
+                className="card-img"
+              />
+              <div className="card-img-text">
+                <h3 className="card-title text-center fs-3 fw-bold align-content-center">
+                  Évènements
+                </h3>
+                <p className="text-dark lead fs-4 text-center">
+                  Vivez une expérience inoubliable !
+                </p>
+              </div>
+              <a className="btn btn-secondary" href="" role="button">
+                View details &raquo;
+              </a>
             </div>
           </div>
         </div>
