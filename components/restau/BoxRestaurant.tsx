ee75ef7f935ed02fe47ede5fafe5b4b635c10d01
LEX-ONE7 <118214894+LEX-ONE7@users.noreply.github.com>
2022-12-14 08:00:28 -1000

    2 files modified

--- components/restau/BoxRestaurant.tsx
+++ components/restau/BoxRestaurant.tsx
@@ -4,7 +4,31 @@ const BoxRestaurant = () => {
     <div className="container-fluid">
       <div className="row">
         {/* Activités */}
-
+        <div className="row align-items-md-stretch">
+        <div className="col-md-6">
+          <div className="h-100 p-5 text-bg-dark rounded-3 text-center">
+            <img
+              style={{ height: '450px' }}
+              className="img-fluid w-100"
+              src="https://www.yestahiti.fr/_client_files/infos_voyageurs/chaud-froid.jpg"
+            />
+            <h2 className="mt-4 mb-4">Chaud-Froid</h2>
+            <p className="fs-5">Tataki au thon rouge.</p>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="h-100 p-5 text-bg-dark border rounded-3 text-center">
+            <img
+              style={{ height: '450px' }}
+              className="img-fluid w-100"
+              src="https://img.theculturetrip.com/1440x/smart/wp-content/uploads/2020/02/feet5t.jpg"
+            />
+            <h2 className="mt-4 mb-4">Crudités</h2>
+            <p className="fs-5">Poisson cru au lait de coco.</p>
+          </div>
+        </div>
+      </div>
+      
         <div className="container">
           <div className="row p-2">
             <div className="col-md-4 text-center card rounded-5">
--- components/rooms/BoxBungalow.tsx
+++ components/rooms/BoxBungalow.tsx
@@ -46,31 +46,7 @@ const BoxBungalow = () => (
         </div>
       </div>
 
-      <div className="row align-items-md-stretch">
-        <div className="col-md-6">
-          <div className="h-100 p-5 text-bg-dark rounded-3 text-center">
-            <img
-              style={{ height: '450px' }}
-              className="img-fluid w-100"
-              src="https://www.yestahiti.fr/_client_files/infos_voyageurs/chaud-froid.jpg"
-            />
-            <h2 className="mt-4 mb-4">Chaud-Froid</h2>
-            <p className="fs-5">Tataki au thon rouge.</p>
-          </div>
-        </div>
-        <div className="col-md-6">
-          <div className="h-100 p-5 text-bg-dark border rounded-3 text-center">
-            <img
-              style={{ height: '450px' }}
-              className="img-fluid w-100"
-              src="https://img.theculturetrip.com/1440x/smart/wp-content/uploads/2020/02/feet5t.jpg"
-            />
-            <h2 className="mt-4 mb-4">Crudités</h2>
-            <p className="fs-5">Poisson cru au lait de coco.</p>
-          </div>
-        </div>
-      </div>
-      
+    
 
         <footer className="pt-3 mt-4 text-muted border-top">&copy; 2022</footer>
       </div>
